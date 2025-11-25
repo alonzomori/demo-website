db81abac59936b46fe4d66b17beb6efa6dd85949
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-11-25 14:44:21 +0900

    Resized Logo

--- demo-website/src/components/Header.jsx
+++ demo-website/src/components/Header.jsx
@@ -4,7 +4,7 @@ import DueTimeProductions from '../assets/images/dtplogo.png'
 function Header() {
     return (
         <div className='fixed top-0 left-0 m-4'>
-            <img className='w-32 sm:w-48 md:w-64 lg:w-80 h-auto mb-4' src={DueTimeProductions} alt="Due Time Productions Logo" />
+            <img className='w-30 sm:w-30 md:w-30 lg:w-30 h-auto mb-4' src={DueTimeProductions} alt="Due Time Productions Logo" />
         </div>
     )
 }
